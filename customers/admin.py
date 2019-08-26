3fd16bb448cc47c361ef38340debede335f9a9d4
ralphmujar <ralphmujar@yahoo.com>
2019-08-26 01:43:41 +0000

    items

--- customers/admin.py
+++ customers/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, Item, \
+        Order
 
 
 admin.site.register(Customer)
+admin.site.register(Item)
+admin.site.register(Order)
--- customers/models.py
+++ customers/models.py
@@ -12,3 +12,22 @@ class Customer(models.Model):
     def __str__(self):
         return self.first_name
 
+class Item(models.Model):
+    
+    name = models.CharField(max_length=75)
+    price = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    qty = models.IntegerField(default=0)
+    price = models.FloatField(default=0)
+    status = models.CharField(max_length=75, default='on process')
+    date = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.item.name
