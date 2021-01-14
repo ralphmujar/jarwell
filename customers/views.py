af3d92adc4d3a051ae15cc37fb98ccd36c57df2d
ralph william mujar <ralphwilliammujar@ralphs-MacBook-Air.local>
2021-01-14 12:29:43 +0800

    templates

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.contrib.messages.views import SuccessMessageMixin
@@ -5,21 +6,27 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 
 #views
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 #models
-from .models import Customer, Item, Order
+from .models import Customer, Item, Order, OrderItem
 
 #forms
-from .forms import CustomerForm, ItemForm, OrderForm
+from .forms import CustomerForm, ItemForm, OrderForm, \
+        OrderItemFormset 
 
 #decorator
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 from .models import Customer, Item, Order
+
+#formset
+from django.forms import inlineformset_factory
+#atomic
+from django.db import transaction
 
 @login_required(login_url='/accounts/login')
 def dashboard(request):
@@ -73,6 +80,11 @@ class ItemsList(ListView):
         return Item.objects.order_by('-date_encoded')
 
 
+class ItemsDetail(DetailView):
+    model = Item
+    template_name = 'customers/item_details.html'
+
+
 @method_decorator(login_required, name='dispatch')
 class ItemCreate(CreateView):
     model = Item
@@ -98,24 +110,71 @@ class OrderList(ListView):
         return Order.objects.order_by('-date')
 
 
+class OrderDetail(DetailView):
+    model = Order
+    template_name = 'customers/order_details.html'
+
+
 
 
 
 @method_decorator(login_required, name='dispatch')
 class OrderCreate(CreateView):
     model = Order
-    form_class = OrderForm
+    fields = ['customer', 'shipping_fee', 'shipping_status', 'notes']
     template_name = 'customers/order_create.html'
     success_url = reverse_lazy('customers:orders')
 
+    def get_context_data(self, **kwargs):
+        data = super(OrderCreate, self).get_context_data(**kwargs)
+        if self.request.POST:
+            data['items'] = OrderItemFormset(self.request.POST)
+        else:
+            data['items'] = OrderItemFormset()
+        return data
 
-class OrderUpdate(UpdateView):
+    def form_valid(self, form):
+        context = self.get_context_data()
+        items = context['items']
+        with transaction.atomic():
+            self.object = form.save()
+
+            if items.is_valid():
+                items.instance = self.object
+                items.save()
+
+        return super(OrderCreate, self).form_valid(form)
+
+
+
+class OrderDelete(DeleteView):
     model = Order
-    form_class = OrderForm
-    template_name = 'customers/order_create.html'
+    fields = ['customer']
     success_url = reverse_lazy('customers:orders')
 
 
+
+def order_update(request, order_id):
+
+    order = Order.objects.get(id=order_id)
+    form = OrderForm(request.POST, instance=order)
+    ItemFormset = inlineformset_factory(Order, OrderItem, form=OrderForm, extra=1)
+
+    if request.method == 'POST':
+
+        formset = ItemFormset(request.POST, instance=order)
+
+        if formset.is_valid():
+            form.save()
+            formset.save()
+            from django.contrib import messages
+            messages.success(request, 'Order successfully updated')
+    else:
+
+        form = OrderForm(instance=order)
+        formset = ItemFormset(instance=order)
+
+    return render(request, 'customers/order_update.html', {'form':form, 'formset' : formset})
 
 
 
@@ -131,3 +190,12 @@ def get_item_price(request):
         total_price = ( (item.price * float(qty) ) + float(shipping_fee) )
         return JsonResponse(total_price, safe=False)
 
+
+
+
+
+
+
+
+#def get_item(request):
+    
